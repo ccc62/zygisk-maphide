8dc18dbe96fc66c5d30770f91286c9d1d0e3ed95
ccc <81462816+ccc62@users.noreply.github.com>
2024-09-05 01:49:19 +0800

    Update zygisk.cpp

--- native/jni/zygisk.cpp
+++ native/jni/zygisk.cpp
@@ -22,18 +22,28 @@
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__)
 
-static void hide_from_maps(std::vector<lsplt::MapInfo> maps) {
+static void hide_from_maps(std::vector<lsplt::MapInfo> &maps) {
     for (auto &info : maps) {
         LOGI("hide: %s\n", info.path.data());
         void *addr = reinterpret_cast<void *>(info.start);
         size_t size = info.end - info.start;
         void *copy = mmap(nullptr, size, PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        if (!copy || copy == MAP_FAILED) {
+            LOGE("mmap failed: %s\n", strerror(errno));
+            continue;
+        }
         if ((info.perms & PROT_READ) == 0) {
             mprotect(addr, size, PROT_READ);
         }
         memcpy(copy, addr, size);
-        mremap(copy, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, addr);
-        mprotect(addr, size, info.perms);
+        void *new_addr = mremap(copy, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, addr);
+        if (new_addr == MAP_FAILED) {
+            LOGE("mremap failed: %s\n", strerror(errno));
+            munmap(copy, size);
+            continue;
+        }
+        mprotect(new_addr, size, info.perms);
+        madvise(new_addr, size, MADV_DONTDUMP);
     }
 }
 
@@ -50,9 +60,9 @@ public:
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
         uint32_t flags = api->getFlags();
-    	if ((flags & zygisk::PROCESS_ON_DENYLIST) && args->uid > 1000) {
+        if ((flags & zygisk::PROCESS_ON_DENYLIST) && args->uid > 1000) {
             DoHide();
-    	}
+        }
         api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
     }
 
@@ -65,7 +75,7 @@ private:
     JNIEnv *env;
 
     void DoHide() {
-    	auto maps = lsplt::MapInfo::Scan();
+        auto maps = lsplt::MapInfo::Scan();
         struct stat st;
         if (stat("/data", &st)) return;
 
@@ -75,10 +85,10 @@ private:
                 LOGI("Hiding Termux related mapping: %s\n", iter->path.data());
                 iter = maps.erase(iter);
             } else if (iter->dev != st.st_dev || 
-            (!(iter->path).starts_with("/system/") &&
-             !(iter->path).starts_with("/vendor/") &&
-             !(iter->path).starts_with("/product/") &&
-             !(iter->path).starts_with("/system_ext/"))) {
+                       !(iter->path.starts_with("/system/") ||
+                         iter->path.starts_with("/vendor/") ||
+                         iter->path.starts_with("/product/") ||
+                         iter->path.starts_with("/system_ext/"))) {
                 iter = maps.erase(iter);
             } else {
                 ++iter;
