a5db8d78e1b1cb94bce8d61157d7d44d1a6ca2f7
ccc <81462816+ccc62@users.noreply.github.com>
2024-09-09 18:29:09 +0800

    Update zygisk.cpp

--- native/jni/zygisk.cpp
+++ native/jni/zygisk.cpp
@@ -22,51 +22,30 @@
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__)
 
-static void hide_from_maps(std::vector<lsplt::MapInfo> &maps) {
-    for (auto &info : maps) {
-        LOGI("hide: %s\n", info.path.data());
-        void *addr = reinterpret_cast<void *>(info.start);
-        size_t size = info.end - info.start;
-        void *copy = mmap(nullptr, size, PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-        if (!copy || copy == MAP_FAILED) {
-            LOGE("mmap failed: %s\n", strerror(errno));
-            continue;
-        }
-        if ((info.perms & PROT_READ) == 0) {
-            mprotect(addr, size, PROT_READ);
-        }
-        memcpy(copy, addr, size);
-        void *new_addr = mremap(copy, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, addr);
-        if (new_addr == MAP_FAILED) {
-            LOGE("mremap failed: %s\n", strerror(errno));
-            munmap(copy, size);
-            continue;
-        }
-        mprotect(new_addr, size, info.perms);
-        madvise(new_addr, size, MADV_DONTDUMP);
-    }
-}
-
-using zygisk::Api;
-using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
-
 class MapHide : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
         this->api = api;
         this->env = env;
+        LOGI("Zygisk 模块已加载");
     }
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
         uint32_t flags = api->getFlags();
         if ((flags & zygisk::PROCESS_ON_DENYLIST) && args->uid > 1000) {
+            LOGI("执行模块隐藏操作...");
             DoHide();
         }
         api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
     }
 
     void preServerSpecialize(ServerSpecializeArgs *args) override {
+        LOGI("system_server 进程启动");
+        int fd = api->connectCompanion();  // 创建伴生进程
+        if (fd != -1) {
+            close(fd);
+            LOGI("Root 伴生进程连接成功");
+        }
         api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
     }
 
@@ -78,17 +57,15 @@ private:
         auto maps = lsplt::MapInfo::Scan();
         struct stat st;
         if (stat("/data", &st)) return;
-
-        // 隐藏与 com.termux 相关的内存映射
+        // hide module file from maps
+        // detection: https://github.com/vvb2060/MagiskDetector/blob/master/README_ZH.md
+        // hide all maps with path is data partition but path is not /data/*
         for (auto iter = maps.begin(); iter != maps.end();) {
-            if (iter->path.find("com.termux") != std::string::npos) {
-                LOGI("Hiding Termux related mapping: %s\n", iter->path.data());
-                iter = maps.erase(iter);
-            } else if (iter->dev != st.st_dev || 
-                       !(iter->path.starts_with("/system/") ||
-                         iter->path.starts_with("/vendor/") ||
-                         iter->path.starts_with("/product/") ||
-                         iter->path.starts_with("/system_ext/"))) {
+            if (iter->dev != st.st_dev ||
+            (!(iter->path).starts_with("/system/") &&
+             !(iter->path).starts_with("/vendor/") &&
+             !(iter->path).starts_with("/product/") &&
+             !(iter->path).starts_with("/system_ext/"))) {
                 iter = maps.erase(iter);
             } else {
                 ++iter;
@@ -96,10 +73,29 @@ private:
         }
         hide_from_maps(maps);
     }
+
+    static void hide_from_maps(std::vector<lsplt::MapInfo> maps) {
+        for (auto &info : maps) {
+            LOGI("hide: %s\n", info.path.data());
+            void *addr = reinterpret_cast<void *>(info.start);
+            size_t size = info.end - info.start;
+            void *copy = mmap(nullptr, size, PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+            if ((info.perms & PROT_READ) == 0) {
+                mprotect(addr, size, PROT_READ);
+            }
+            memcpy(copy, addr, size);
+            mremap(copy, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, addr);
+            mprotect(addr, size, info.perms);
+        }
+    }
 };
 
-static void companion_handler(int i) {
-    return;
+// 伴生进程处理函数
+static void companion_handler(int fd) {
+    LOGI("Root 伴生进程启动");
+    // 在这里添加 Root 权限操作
+    // 可以处理高权限的操作，例如操作系统文件、隐藏模块等
+    close(fd);
 }
 
 REGISTER_ZYGISK_MODULE(MapHide)
